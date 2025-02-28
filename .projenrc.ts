b0bd2e5e4f05c4c21e8b6fa0c49fe6bf2ce81b67
Ajitha Estharla <aestharl@amazon.com>
2025-02-28 13:25:34 -0800

    feat: initial projen package build

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,32 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Ajitha Estharla',
-  authorAddress: 'aestharl@amazon.com',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.7.0',
-  name: 'framework-for-github-app-on-aws',
+  author: "Amazon OSPO",
+  authorAddress: "osa-dev+puzzleglue@amazon.com",
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  jsiiVersion: "~5.7.0",
+  name: "framework-for-github-app-on-aws",
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/ajestharl/framework-for-github-app-on-aws.git',
+  repositoryUrl:
+    "https://github.com/ajestharl/framework-for-github-app-on-aws.git",
+  docgen: true,
+  github: true,
+  gitignore: [".idea"],
+  eslint: true,
+  eslintOptions: {
+    prettier: true,
+    fileExtensions: [".ts", ".md"],
+    dirs: ["src", "test", "docs"],
+  },
+  jestOptions: {
+    jestConfig: {
+      verbose: true,
+    },
+  },
+  cdkVersionPinning: false,
+  release: false,
+  autoMerge: false,
+  releaseToNpm: false,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="framework-for-github-app-on-aws.Hello"></a>
+
+#### Initializers <a name="Initializers" id="framework-for-github-app-on-aws.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'framework-for-github-app-on-aws'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#framework-for-github-app-on-aws.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="framework-for-github-app-on-aws.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
