4fe8aa79aaa85b4ee8334296ed5110e515d04131
Emi Aimi <emiaimi@amazon.com>
2025-03-06 23:22:06 -0800

    fix: fix eslint *.md error
    # Conflicts:
    #	.projenrc.ts

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { awscdk } from "projen";
+import { awscdk, JsonFile } from "projen";
 export const project = new awscdk.AwsCdkConstructLibrary({
   author: "Amazon OSPO",
   authorAddress: "osa-dev+puzzleglue@amazon.com",
@@ -33,10 +33,6 @@ export const project = new awscdk.AwsCdkConstructLibrary({
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
-project.eslint?.addOverride({
-  files: ["*.md"],
-  parser: "markdown-eslint-parser",
-});
 if (project.github) {
   const buildWorkflow = project.github?.tryFindWorkflow("build");
   if (buildWorkflow && buildWorkflow.file) {
@@ -44,7 +40,7 @@ if (project.github) {
   }
 }
 
-new awscdk.AwsCdkTypeScriptApp({
+const genetFramework = new awscdk.AwsCdkTypeScriptApp({
   parent: project,
   outdir: "src/packages/genet-framework",
   cdkVersion: "2.1.0",
@@ -55,6 +51,43 @@ new awscdk.AwsCdkTypeScriptApp({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+});
+new JsonFile(genetFramework, ".prettierrc.json", {
+  obj: {
+    singleQuote: true,
+    trailingComma: "all",
+  },
+});
+genetFramework.addDevDeps(
+  "eslint-plugin-md",
+  "markdown-eslint-parser",
+  "eslint-plugin-prettier",
+);
+genetFramework.eslint?.addExtends(
+  "plugin:md/recommended",
+  "plugin:prettier/recommended",
+);
+genetFramework.eslint?.addOverride({
+  files: ["*.md"],
+  parser: "markdown-eslint-parser",
+  rules: {
+    "prettier/prettier": ["error", { parser: "markdown" }],
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/return-await": "off",
+    quotes: "off",
+  },
+});
+genetFramework.eslint?.addRules({
+  "prettier/prettier": "error",
+  "md/remark": [
+    "error",
+    {
+      plugins: [
+        "preset-lint-markdown-style-guide",
+        ["lint-list-item-indent", "space"],
+      ],
+    },
+  ],
 });
 
 project.synth();
