7a6c40705eef6c8bf07a257d43141f101d921b6b
Emi Aimi <emiaimi@amazon.com>
2025-03-06 23:22:06 -0800

    fix: fix eslint *.md error

--- src/packages/genet-framework/src/credential-manager/index.ts
+++ src/packages/genet-framework/src/credential-manager/index.ts
@@ -1,8 +1,7 @@
 import { RemovalPolicy, NestedStack } from 'aws-cdk-lib';
 import { AttributeType, Table, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-export interface CredentialManagerProps {
-}
+export interface CredentialManagerProps {}
 
 /**
  * Nested stack for storing GitHub App installation targets and pre-approved lists.
--- src/packages/genet-framework/src/installation-manager/index.ts
+++ src/packages/genet-framework/src/installation-manager/index.ts
@@ -1,8 +1,7 @@
 import { NestedStack } from 'aws-cdk-lib';
 import { AttributeType, Table, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-export interface InstallationManagerProps {
-}
+export interface InstallationManagerProps {}
 /**
  * Nested stack for managing GitHub App installation targets and pre-approved lists.
  */
