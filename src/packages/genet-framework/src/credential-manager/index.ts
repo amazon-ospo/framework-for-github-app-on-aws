ab23ad544c8e1c0ef92244acea8d3732753dff50
Emi Aimi <emiaimi@amazon.com>
2025-03-13 15:53:53 -0700

    feat: move installation table to credential manager component

--- src/packages/genet-framework/src/credential-manager/index.ts
+++ src/packages/genet-framework/src/credential-manager/index.ts
@@ -8,6 +8,7 @@ export interface CredentialManagerProps {}
  */
 export class CredentialManager extends NestedStack {
   readonly appTable: Table;
+  readonly installationTable: Table;
   constructor(scope: Construct, id: string, props?: CredentialManagerProps) {
     super(scope, id, props);
     Tags.of(this).add('GenetComponent', 'CredentialManager');
@@ -22,5 +23,48 @@ export class CredentialManager extends NestedStack {
       pointInTimeRecoverySpecification: { pointInTimeRecoveryEnabled: true },
     });
     Tags.of(this.appTable).add('CredentialManager', 'AppTable');
+    // Table for tracking GitHub App installations.
+    // Stores `node_id`, `installation_id`, `app_id`
+    // and last updated timestamp.
+    this.installationTable = new Table(this, 'AppInstallationTable', {
+      partitionKey: {
+        name: 'AppId',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'NodeId',
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      pointInTimeRecoverySpecification: { pointInTimeRecoveryEnabled: true },
+    });
+    Tags.of(this.installationTable).add(
+      'CredentialManager',
+      'AppInstallationTable',
+    );
+    // Global secondary index for looking up installations by Node ID.
+    this.installationTable.addGlobalSecondaryIndex({
+      indexName: 'NodeID',
+      partitionKey: {
+        name: 'NodeId',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'AppId',
+        type: AttributeType.STRING,
+      },
+    });
+    // Global secondary index for looking up installations by installations ID.
+    this.installationTable.addGlobalSecondaryIndex({
+      indexName: 'InstallationID',
+      partitionKey: {
+        name: 'InstallationId',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'AppId',
+        type: AttributeType.STRING,
+      },
+    });
   }
 }
