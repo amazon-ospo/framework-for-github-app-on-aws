1155cf446ffff09a905b0ea5f9cde8296e6ee550
Ajitha Estharla <aestharl@amazon.com>
2025-03-17 11:24:47 -0700

    feat: Add genet-ops-tools package and common package confirguration setup

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,22 +1,64 @@
-import { awscdk, JsonFile } from "projen";
+import { awscdk, JsonFile } from 'projen';
+import { TypeScriptAppProject } from 'projen/lib/typescript';
+
+export const configureMarkDownLinting = (tsProject: TypeScriptAppProject) => {
+  new JsonFile(tsProject, '.prettierrc.json', {
+    obj: {
+      singleQuote: true,
+      trailingComma: 'all',
+    },
+  });
+  tsProject.addDevDeps(
+    'eslint-plugin-md',
+    'markdown-eslint-parser',
+    'eslint-plugin-prettier',
+  );
+  tsProject.eslint?.addExtends(
+    'plugin:md/recommended',
+    'plugin:prettier/recommended',
+  );
+  tsProject.eslint?.addOverride({
+    files: ['*.md'],
+    parser: 'markdown-eslint-parser',
+    rules: {
+      'prettier/prettier': ['error', { parser: 'markdown' }],
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/return-await': 'off',
+      quotes: 'off',
+    },
+  });
+  tsProject.eslint?.addRules({
+    'prettier/prettier': 'error',
+    'md/remark': [
+      'error',
+      {
+        plugins: [
+          'preset-lint-markdown-style-guide',
+          ['lint-list-item-indent', 'space'],
+        ],
+      },
+    ],
+  });
+};
+
 export const project = new awscdk.AwsCdkConstructLibrary({
-  author: "Amazon OSPO",
-  authorAddress: "osa-dev+puzzleglue@amazon.com",
-  cdkVersion: "2.1.0",
-  defaultReleaseBranch: "main",
-  jsiiVersion: "~5.7.0",
-  name: "framework-for-github-app-on-aws",
+  author: 'Amazon OSPO',
+  authorAddress: 'osa-dev+puzzleglue@amazon.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.7.0',
+  name: 'framework-for-github-app-on-aws',
   projenrcTs: true,
   repositoryUrl:
-    "https://github.com/amazon-ospo/framework-for-github-app-on-aws.git",
+    'https://github.com/amazon-ospo/framework-for-github-app-on-aws.git',
   docgen: true,
   github: true,
-  gitignore: [".idea"],
+  gitignore: ['.idea'],
   eslint: true,
   eslintOptions: {
     prettier: true,
-    fileExtensions: [".ts", ".md"],
-    dirs: ["src", "test", "docs"],
+    fileExtensions: ['.ts', '.md'],
+    dirs: ['src', 'test', 'docs'],
   },
   jestOptions: {
     jestConfig: {
@@ -28,100 +70,48 @@ export const project = new awscdk.AwsCdkConstructLibrary({
   autoMerge: false,
   releaseToNpm: false,
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  // deps: [],                /* Runtime dependencies of this module. /
+  // description: undefined,  / The description is just a string that helps people understand the purpose of the package. /
+  // devDeps: [],             / Build dependencies for this module. /
+  // packageName: undefined,  / The "name" in package.json. */
 });
-
-project.addDevDeps(
-  "eslint-plugin-md",
-  "markdown-eslint-parser",
-  "eslint-plugin-prettier",
-);
-project.eslint?.addExtends(
-  "plugin:md/recommended",
-  "plugin:prettier/recommended",
-);
-project.eslint?.addOverride({
-  files: ["*.md"],
-  parser: "markdown-eslint-parser",
-  rules: {
-    "prettier/prettier": ["error", { parser: "markdown" }],
-    "@typescript-eslint/no-floating-promises": "off",
-    "@typescript-eslint/return-await": "off",
-    quotes: "off",
-  },
-});
-project.eslint?.addRules({
-  "prettier/prettier": "error",
-  "md/remark": [
-    "error",
-    {
-      plugins: [
-        "preset-lint-markdown-style-guide",
-        ["lint-list-item-indent", "space"],
-      ],
-    },
-  ],
-});
-
 if (project.github) {
-  const buildWorkflow = project.github?.tryFindWorkflow("build");
+  const buildWorkflow = project.github?.tryFindWorkflow('build');
   if (buildWorkflow && buildWorkflow.file) {
-    buildWorkflow.file.addOverride("jobs.build.permissions.contents", "read");
+    buildWorkflow.file.addOverride('jobs.build.permissions.contents', 'read');
   }
 }
+configureMarkDownLinting(project);
 
-const genetFramework = new awscdk.AwsCdkTypeScriptApp({
-  parent: project,
-  outdir: "src/packages/genet-framework",
-  cdkVersion: "2.1.0",
-  defaultReleaseBranch: "main",
-  name: "genet-framework",
-  projenrcTs: true,
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
-});
-new JsonFile(genetFramework, ".prettierrc.json", {
-  obj: {
-    singleQuote: true,
-    trailingComma: "all",
-  },
-});
+interface PackageConfig {
+  name: string;
+  outdir: string;
+  deps?: string[];
+  devDeps?: string[];
+}
 
-genetFramework.addDevDeps(
-  "eslint-plugin-md",
-  "markdown-eslint-parser",
-  "eslint-plugin-prettier",
-);
-genetFramework.eslint?.addExtends(
-  "plugin:md/recommended",
-  "plugin:prettier/recommended",
-);
-genetFramework.eslint?.addOverride({
-  files: ["*.md"],
-  parser: "markdown-eslint-parser",
-  rules: {
-    "prettier/prettier": ["error", { parser: "markdown" }],
-    "@typescript-eslint/no-floating-promises": "off",
-    "@typescript-eslint/return-await": "off",
-    quotes: "off",
-  },
+export const createPackage = (config: PackageConfig) => {
+  const tsProject = new awscdk.AwsCdkTypeScriptApp({
+    parent: project,
+    outdir: config.outdir,
+    cdkVersion: '2.1.0',
+    defaultReleaseBranch: 'main',
+    name: config.name,
+    projenrcTs: false,
+    deps: config.deps || [],
+    devDeps: config.devDeps || [],
+  });
+  configureMarkDownLinting(tsProject);
+  return tsProject;
+};
+
+createPackage({
+  name: 'genet-framework',
+  outdir: 'src/packages/genet-framework',
 });
-genetFramework.eslint?.addRules({
-  "prettier/prettier": "error",
-  "md/remark": [
-    "error",
-    {
-      plugins: [
-        "preset-lint-markdown-style-guide",
-        ["lint-list-item-indent", "space"],
-      ],
-    },
-  ],
+createPackage({
+  name: 'genet-ops-tools',
+  outdir: 'src/packages/genet-ops-tools',
 });
 
 project.synth();
