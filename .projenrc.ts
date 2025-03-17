e894c930366a80a05d8ffe99655ba23a66a18423
Ajitha Estharla <aestharl@amazon.com>
2025-03-17 12:14:49 -0700

    feat: Changed single quote to double quote for strings

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,34 +1,34 @@
-import { awscdk, JsonFile } from 'projen';
-import { TypeScriptAppProject } from 'projen/lib/typescript';
+import { awscdk, JsonFile } from "projen";
+import { TypeScriptAppProject } from "projen/lib/typescript";
 
 export const configureMarkDownLinting = (tsProject: TypeScriptAppProject) => {
   tsProject.addDevDeps(
-    'eslint-plugin-md',
-    'markdown-eslint-parser',
-    'eslint-plugin-prettier',
+    "eslint-plugin-md",
+    "markdown-eslint-parser",
+    "eslint-plugin-prettier",
   );
   tsProject.eslint?.addExtends(
-    'plugin:md/recommended',
-    'plugin:prettier/recommended',
+    "plugin:md/recommended",
+    "plugin:prettier/recommended",
   );
   tsProject.eslint?.addOverride({
-    files: ['*.md'],
-    parser: 'markdown-eslint-parser',
+    files: ["*.md"],
+    parser: "markdown-eslint-parser",
     rules: {
-      'prettier/prettier': ['error', { parser: 'markdown' }],
-      '@typescript-eslint/no-floating-promises': 'off',
-      '@typescript-eslint/return-await': 'off',
-      quotes: 'off',
+      "prettier/prettier": ["error", { parser: "markdown" }],
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/return-await": "off",
+      quotes: "off",
     },
   });
   tsProject.eslint?.addRules({
-    'prettier/prettier': 'error',
-    'md/remark': [
-      'error',
+    "prettier/prettier": "error",
+    "md/remark": [
+      "error",
       {
         plugins: [
-          'preset-lint-markdown-style-guide',
-          ['lint-list-item-indent', 'space'],
+          "preset-lint-markdown-style-guide",
+          ["lint-list-item-indent", "space"],
         ],
       },
     ],
@@ -36,23 +36,23 @@ export const configureMarkDownLinting = (tsProject: TypeScriptAppProject) => {
 };
 
 export const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Amazon OSPO',
-  authorAddress: 'osa-dev+puzzleglue@amazon.com',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.7.0',
-  name: 'framework-for-github-app-on-aws',
+  author: "Amazon OSPO",
+  authorAddress: "osa-dev+puzzleglue@amazon.com",
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  jsiiVersion: "~5.7.0",
+  name: "framework-for-github-app-on-aws",
   projenrcTs: true,
   repositoryUrl:
-    'https://github.com/amazon-ospo/framework-for-github-app-on-aws.git',
+    "https://github.com/amazon-ospo/framework-for-github-app-on-aws.git",
   docgen: true,
   github: true,
-  gitignore: ['.idea'],
+  gitignore: [".idea"],
   eslint: true,
   eslintOptions: {
     prettier: true,
-    fileExtensions: ['.ts', '.md'],
-    dirs: ['src', 'test', 'docs'],
+    fileExtensions: [".ts", ".md"],
+    dirs: ["src", "test", "docs"],
   },
   jestOptions: {
     jestConfig: {
@@ -70,12 +70,12 @@ export const project = new awscdk.AwsCdkConstructLibrary({
   // packageName: undefined,  / The "name" in package.json. */
 });
 if (project.github) {
-  const buildWorkflow = project.github?.tryFindWorkflow('build');
+  const buildWorkflow = project.github?.tryFindWorkflow("build");
   if (buildWorkflow && buildWorkflow.file) {
-    buildWorkflow.file.addOverride('jobs.build.permissions.contents', 'read');
-    buildWorkflow.file.addOverride('jobs.build.env', {
-      CI: 'true',
-      NODE_OPTIONS: '--max-old-space-size=8192',
+    buildWorkflow.file.addOverride("jobs.build.permissions.contents", "read");
+    buildWorkflow.file.addOverride("jobs.build.env", {
+      CI: "true",
+      NODE_OPTIONS: "--max-old-space-size=8192",
     });
   }
 }
@@ -92,17 +92,17 @@ export const createPackage = (config: PackageConfig) => {
   const tsProject = new awscdk.AwsCdkTypeScriptApp({
     parent: project,
     outdir: config.outdir,
-    cdkVersion: '2.1.0',
-    defaultReleaseBranch: 'main',
+    cdkVersion: "2.1.0",
+    defaultReleaseBranch: "main",
     name: config.name,
     projenrcTs: false,
     deps: config.deps || [],
     devDeps: config.devDeps || [],
   });
-  new JsonFile(tsProject, '.prettierrc.json', {
+  new JsonFile(tsProject, ".prettierrc.json", {
     obj: {
       singleQuote: true,
-      trailingComma: 'all',
+      trailingComma: "all",
     },
   });
   configureMarkDownLinting(tsProject);
@@ -110,12 +110,12 @@ export const createPackage = (config: PackageConfig) => {
 };
 
 createPackage({
-  name: 'genet-framework',
-  outdir: 'src/packages/genet-framework',
+  name: "genet-framework",
+  outdir: "src/packages/genet-framework",
 });
 createPackage({
-  name: 'genet-ops-tools',
-  outdir: 'src/packages/genet-ops-tools',
+  name: "genet-ops-tools",
+  outdir: "src/packages/genet-ops-tools",
 });
 
 project.synth();
