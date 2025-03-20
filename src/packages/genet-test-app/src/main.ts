2a65026eb5838f01bcee6130bcca0d39c63a0861
Emi Aimi <emiaimi@amazon.com>
2025-03-20 13:19:30 -0700

    feat: add test for credential and installation manager deployment

--- src/packages/genet-test-app/src/main.ts
+++ src/packages/genet-test-app/src/main.ts
@@ -1,11 +1,15 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CredentialManager, InstallationManager } from 'genet-framework';
 
-export class MyStack extends Stack {
+// CDK App entry for Genet acceptance test.
+// This stack is intended for testing genet-framework library.
+// In a real use case, it should be a stack defined by customer.
+export class GenetFrameworkTestStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
-
-    // define resources here...
+    new CredentialManager(this, 'CredentialManager', {});
+    new InstallationManager(this, 'InstallationManager', {});
   }
 }
 
@@ -17,6 +21,6 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, 'genet-framework-test-stack', { env: devEnv });
+new GenetFrameworkTestStack(app, 'genet-framework-test-stack', { env: devEnv });
 
 app.synth();
--- src/packages/genet-test-app/test/main.test.ts
+++ src/packages/genet-test-app/test/main.test.ts
@@ -1,10 +1,14 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { MyStack } from '../src/main';
+import { GenetFrameworkTestStack } from '../src/main';
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new MyStack(app, 'test');
+  const stack = new GenetFrameworkTestStack(
+    app,
+    'genet-framework-test-stack',
+    {},
+  );
 
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
